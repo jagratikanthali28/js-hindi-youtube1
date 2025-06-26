5d008ca13f66ccc1d63e312db13751c6a164f3d8
jagratikanthali28 <159175028+jagratikanthali28@users.noreply.github.com>
2025-06-26 19:43:32 +0000

    string to number conversion

---
+++ 01_basics/04_stoNumber.js
@@ -0,0 +1,20 @@
+let  val=3;
+let negvalue=-3;
+console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let s1="hello";
+let s2="jaggu";
+let s3=s1+s2;
+console.log(s3);
+
+console.log(1+"2");
+console.log("2"+1);
+console.log("1"+"2");
+console.log(1+2+"3");
+console.log("1"+2+3);
