a1db51b70710cbca2ab8c21c50e1795f07fa0e1c
jagratikanthali28 <159175028+jagratikanthali28@users.noreply.github.com>
2025-06-26 19:15:21 +0000

    type conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,21 @@
+let score=null;
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let val=Number(score);
+//console.log(typeof val);
+//console.log(val);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoogedIn="";
+let booleanisLoggedin=Boolean(isLoogedIn);
+//console.log(booleanisLoggedin);
+//console.log(typeof booleanisLoggedin);
+
+let vale=33;
+let convertval=String(vale);
+console.log(convertval);
+console.log(typeof convertval);
