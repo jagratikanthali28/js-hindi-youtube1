e9706ba3869c553a9c36cb5ab968c5315a36d380
jagratikanthali28 <159175028+jagratikanthali28@users.noreply.github.com>
2025-06-27 21:58:01 +0000

    date

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -6,3 +6,5 @@ console.log(myDate.toString())
 console.log(myDate.toDateString())
 console.log(myDate.toLocaleString())
 console.log(typeof myDate);
+let createDate=new Date(2023,0,23,5,3);
+console.log(createDate.toLocaleString());
