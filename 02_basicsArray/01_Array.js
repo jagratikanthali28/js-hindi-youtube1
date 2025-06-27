dea5deab05204fcdec2eefc40f828fb0d8153b06
jagratikanthali28 <159175028+jagratikanthali28@users.noreply.github.com>
2025-06-27 21:58:30 +0000

    Array in JS

---
+++ 02_basicsArray/01_Array.js
@@ -0,0 +1,28 @@
+//array
+const myArr=[0,1,2,3,4];
+const myHeroes=["shakti","naggg"];
+console.log(myArr);
+console.log(myHeroes[1]);
+myArr.push(6);
+myArr.push(7);
+myArr.pop();
+myArr.unshift(9);
+myArr.shift();
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(6));
+console.log(myArr);
+
+const newArr=myArr.join()
+console.log(newArr);
+
+//slice , splice
+//console.log("A",myArr);
+console.log(myArr);
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log(myArr);
+//console.log("B",myArr);
+
+const myn2=myArr.splice(1,3);
+console.log(myn2);
+console.log(myArr);
---
+++ 02_basicsArray/02_Array.js
@@ -0,0 +1,22 @@
+console.log("Array 2");
+const m1=["jag","meenu","garui","raja"];
+const m2=["kakul","disha","tapu","saloni"]
+//m1.push(m2);
+const ans=m1.concat(m2);
+console.log(m1);
+console.log(m2);
+console.log(ans);
+console.log(m1);
+console.log(m2);
+//console.log(m1[4][0]);
+const allNew=[...m1,...m2];
+console.log(allNew);
+
+console.log(Array.isArray("jagrati"));
+console.log(Array.from("jagrstuvv"));
+console.log(Array.from({name:"jagrati"}));
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3));
+
