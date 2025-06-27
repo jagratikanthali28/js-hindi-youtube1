6f7043614b6a8dc2496be3837f22fd814e8f4aff
jagratikanthali28 <159175028+jagratikanthali28@users.noreply.github.com>
2025-06-27 09:33:28 +0000

    number and maths

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -2,3 +2,26 @@ console.log("strings");
 const name="jagrati"
 const repoCount=50
 console.log(name+repoCount+"value")
+
+//string interpolation
+console.log(`this is my ${name} and my repocount ${repoCount}`);
+
+const gameName= new String('jagartiv')
+console.log(gameName[0]);
+console.log(gameName._proto_);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('r'));
+const newString=gameName.substring(0,4);
+console.log(newString);
+const newString1="   jagrati  ";
+console.log(newString1);
+console.log(newString1.trim());
+const url="https://jagrti.com/Jgarati%20kanthali";
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('meenu'));
+console.log(gameName.split('-'));
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,20 @@
+console.log("number and maths");
+const score=400;
+console.log(score);
+
+const balance=new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNUmber=123.8966
+console.log(otherNUmber.toPrecision(3))
+
+const val=10000000;
+console.log(val.toLocaleString());
+console.log(val.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++Maths++++++++++++++++++++++++
+console.log(Math);
+
