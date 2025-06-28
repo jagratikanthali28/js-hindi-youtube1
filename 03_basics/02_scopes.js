49a7053ef0eb85c5aa9edb87e7740028179347c5
jagratikanthali28 <159175028+jagratikanthali28@users.noreply.github.com>
2025-06-28 11:54:50 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+
+if(true)
+{
+let a=10;
+const b=20
+var c =30
+
+}
+//console.log(a);   error
+//console.log(b);    error
+//console.log(c);
+function one(){
+    const username="jaggu"
+    function two()
+    {
+         const website="youtube"
+         console.log(username);
+    }
+   // console.log(website);
+    two();
+}
+one()
