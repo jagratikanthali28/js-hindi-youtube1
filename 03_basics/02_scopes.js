1cf9a29db743abcf6bedb065469f6b78fc54bc56
jagratikanthali28 <159175028+jagratikanthali28@users.noreply.github.com>
2025-06-29 13:40:00 +0000

    scopes in JS

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,5 +1,5 @@
 
-if(true)
+//if(true)
 {
 let a=10;
 const b=20
@@ -17,6 +17,39 @@ function one(){
          console.log(username);
     }
    // console.log(website);
-    two();
+    //two();
 }
-one()
+//one()
+
+if(true)
+{
+    const username1="jagrati"
+    if(username1==="jagrati")
+    {
+        const website="youtube";
+        console.log(username1+website)
+    }
+    //console.log(website);
+}
+//console.log(username1)
+
+
+//++++++++++++++++++++++interesting+++++++++++++++
+
+console.log(fun(5))   //it will not give any error
+function fun(value)
+{
+    return value+1;
+}
+//const ans=fun(5);
+//console.log(ans);
+
+
+console.log(addTwo(5));  //it will give yuo error can't access before intialization
+
+const addTwo=function(num)
+{
+    return num+2;
+}
+//addTwo(5);
+//console.log(addTwo);
